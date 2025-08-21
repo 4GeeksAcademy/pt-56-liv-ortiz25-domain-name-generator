bea9c177d35b7e5b68fb40f88e43431afc6a679f
Elizabeth Duarte Ortiz <134437416+liv-ortiz25@users.noreply.github.com>
2025-08-21 02:53:15 +0000

    trabajo finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ["the", "our", "my", "her"];
+  const adj = ["great", "big", "dazzling", "small"];
+  const noun = ["jogger", "racoon", "book", "padawan"];
+  const extensions = ["com", "net", "us", "io"];
+
+  for (let i in pronoun) {
+    for (let j in adj) {
+      for (let k in noun) {
+        for (let l in extensions) {
+          document.querySelector("#domain").innerHTML +=
+            `<li>${pronoun[i]}${adj[j]}${noun[k]}.${extensions[l]}</li>`;
+        }
+      }
+    }
+  }
 };
